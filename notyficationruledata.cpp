2bceb5615ae3d618f2db04db038c72325e056b98
Marcin Łapiński <lapinskm@gmail.com>
2016-04-02 21:44:02 +0200

    Minor fixes in NotyficationRuleData
     + Fix typos "commiter" "commiters"
     + Add missing "m_notifyEndOfFailSpree" xml parsing.

--- notyficationruledata.cpp
+++ notyficationruledata.cpp
@@ -17,15 +17,16 @@ NotyficationRuleData::NotyficationRuleData(const QDomElement & ruleDataElement)
     m_jobName = XmlUtils::elementTextByPath(ruleDataElement, "job_name");
     m_notifyFailures = XmlUtils::elementTextByPath(ruleDataElement,"notify_failures").contains("true");
     m_filterByCommiter = XmlUtils::elementTextByPath(ruleDataElement,"filter_by_commiters").contains("true");
-    QDomNodeList commitersNodes = ruleDataElement.elementsByTagName("commiters");
+    QDomNodeList commitersNodes = ruleDataElement.elementsByTagName("committers");
     for (int i =0; i<commitersNodes.size();++i ) {
-        QDomNodeList commiterNodes = commitersNodes.at(i).toElement().elementsByTagName("commiter");
+        QDomNodeList commiterNodes = commitersNodes.at(i).toElement().elementsByTagName("committer");
          for (int j =0; j<commiterNodes.size(); ++j ) {
              m_commiters.append(commiterNodes.at(j).toElement().text());
          }
     }
     m_lastBuildNumber = XmlUtils::elementTextByPath(ruleDataElement,"last_build_number").toInt();
     m_lastResult = static_cast<BuildResult>(XmlUtils::elementTextByPath(ruleDataElement,"last_buid_result").toInt());
+    m_notifyEndOfFailSpree = XmlUtils::elementTextByPath(ruleDataElement,"notify_end_of_fail_spree").contains("true");
 }
 
 QDomElement NotyficationRuleData::toXml(QDomDocument &doc) const
@@ -35,14 +36,14 @@ QDomElement NotyficationRuleData::toXml(QDomDocument &doc) const
     XmlUtils::appendTextElement(doc, ruleDataElement, "job_name", m_jobName);
     XmlUtils::appendTextElement(doc, ruleDataElement, "notify_failures", m_notifyFailures ? "true" : "false");
     XmlUtils::appendTextElement(doc, ruleDataElement, "filter_by_commiters", m_filterByCommiter ? "true" : "false");
-    QDomElement commitersElement = doc.createElement("commiters");
+    QDomElement commitersElement = doc.createElement("committers");
     foreach (QString commiter, m_commiters) {
-        XmlUtils::appendTextElement(doc, commitersElement, "commiter", commiter);
+        XmlUtils::appendTextElement(doc, commitersElement, "committer", commiter);
     }
     ruleDataElement.appendChild(commitersElement);
     XmlUtils::appendTextElement(doc, ruleDataElement, "last_build_number", QString::number(m_lastBuildNumber));
     XmlUtils::appendTextElement(doc, ruleDataElement, "last_buid_result", QString::number(m_lastResult));
-
+    XmlUtils::appendTextElement(doc, ruleDataElement, "notify_end_of_fail_spree", m_notifyEndOfFailSpree ? "true" : "false");
     return ruleDataElement;
 }
 
